fc41e4809761b6c41030f402deb79577ed968658
Klara Haas <klara-h@gmx.net>
2024-03-07 11:07:40 +0100

    Completed preprocess + synthetic data

--- data_preproc/main.py
+++ data_preproc/main.py
@@ -5,17 +5,19 @@ import seaborn as sns
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import set_config; set_config(display = "diagram")
-from ml_logic.data import clean_data, preprocess_proteins_age_gen, preprocess_proteins_all
-
+from ml_logic.data import clean_data, preprocess_proteins_age_gen, preprocess_proteins_all, synthetic_data
+from sklearn.model_selection import train_test_split
 
 def preprocess_age_gen():
     """
     - Query raw dataset from 'raw_data' folder
+    - split the data into train (60 %), val (20 %), test (20 %)
+    - create synthetic data using SMOTE on training set
     - process data (proteins, age and gender)
     - Store processed data
     -
     """
-    print("✅ Use case: preprocess proteins, age and gender: ")
+    print(" Preprocess proteins, age and gender ")
 
     # Retrieve data
     file_path=os.path.join(os.path.dirname(__file__), '..','raw_data','brain_proteomics_data.csv')
@@ -24,14 +26,31 @@ def preprocess_age_gen():
     #Process data
     data_clean= clean_data(data)
 
+
     # define X and y
-    X_preproc = preprocess_proteins_age_gen(data)
+    X = data_clean.drop(['Case', 'histological_type', 'race', 'ethnicity', 'radiation_therapy', 'Grade', 'Mutation.Count', 'Percent.aneuploidy', 'IDH.status', 'outcome'], axis = 1)
     y= data_clean['outcome']
 
-    print("✅ preprocess() done of proteins, age and gender \n")
-    return X_preproc, y
+    # train-test/val split -- 35 %
+    X_train, X_val_test, y_train, y_val_test = train_test_split(X, y, test_size=0.35, stratify=y)
+    print (f"✅ training data splitted. Size : {X_train.shape}")
 
-#X_preproc, y =preprocess_age_gen()
+    # test-val split -- 50 %
+    X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, stratify=y_val_test)
+    print (f"✅ testing data splitted. Size: {X_test.shape} ")
+    print (f"✅ validation data splitted. Size: {X_val.shape}")
+
+    # produce synthetic data with SMOTE on training set
+    X_train, y_train = synthetic_data(X_train, y_train)
+
+    # preprocess X_train, X_test and X_val
+    X_train, X_val, X_test = preprocess_proteins_age_gen(X_train, X_val, X_test)
+
+    print("✅ preprocess() done of proteins, age and gender \n")
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+preprocess_age_gen()
 
 def preprocess_all():
     """
--- data_preproc/ml_logic/data.py
+++ data_preproc/ml_logic/data.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
 from sklearn import set_config
 set_config(display = 'diagram')
 from sklearn.compose import make_column_transformer, make_column_selector
+from imblearn.over_sampling import SMOTENC
 
 
 def clean_data (df: pd.DataFrame) -> pd.DataFrame:
@@ -19,17 +20,16 @@ def clean_data (df: pd.DataFrame) -> pd.DataFrame:
 
     #clinical_data= ['Case', 'years_to_birth', 'gender', 'histological_type', 'race', 'ethnicity', 'radiation_therapy', 'Grade', 'Mutation.Count', 'Percent.aneuploidy', 'IDH.status', 'outcome' ]
 
-    print("✅ data cleaned and scaled")
+    print("✅ Data cleaned")
     return df
 
-def preprocess_proteins_age_gen(df: pd.DataFrame) ->np.ndarray:
+def preprocess_proteins_age_gen(X_train, X_val, X_test):
     """
     - transform cleaned dataset with MinMaxScaler for all lfq-intensities of proteins.
     - MinMaxScaler for age
     - Oridnal Encoder for gender
 
     """
-    prot_age_gen = df.drop(['Case', 'histological_type', 'race', 'ethnicity', 'radiation_therapy', 'Grade', 'Mutation.Count', 'Percent.aneuploidy', 'IDH.status', 'outcome'], axis = 1)
     preproc_numerical = make_pipeline(
     MinMaxScaler()
     )
@@ -41,9 +41,11 @@ def preprocess_proteins_age_gen(df: pd.DataFrame) ->np.ndarray:
     (preproc_numerical, make_column_selector(dtype_include=["int64", "float64"]))
     )
 
-    X_preproc = preproc_base.fit_transform(prot_age_gen)
+    X_train = preproc_base.fit_transform(X_train)
+    X_val = preproc_base.transform(X_val)
+    X_test= preproc_base.transform(X_test)
 
-    return X_preproc
+    return X_train, X_val, X_test
 
 def preprocess_proteins_all(df: pd.DataFrame) ->np.ndarray:
     """
@@ -69,3 +71,16 @@ def preprocess_proteins_all(df: pd.DataFrame) ->np.ndarray:
     X_preproc = preproc_all.fit_transform(prot_all)
 
     return X_preproc
+
+def synthetic_data(X, y) -> np.array:
+    """
+    - Create training data with SMOTE.
+    - n_samples = 200 -> total number of data points for each class
+    - output will be X_train and y_train equally balanced 200 samples per class (1 and 0)
+    """
+    sampler = SMOTENC(categorical_features=[0,1], sampling_strategy={0: y.value_counts()[0]*2, 1: y.value_counts()[1]*2})
+    X_train, y_train = sampler.fit_resample(X, y)
+
+    print (f"✅ synthetic data created on training set. Size of new training set: {X_train.shape}")
+
+    return X_train, y_train
