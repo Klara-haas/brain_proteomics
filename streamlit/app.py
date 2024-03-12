3f5ffca6bc61a693b5d35dd6d26d51b34f06185a
Klara Haas <155819144+Klara-haas@users.noreply.github.com>
2024-03-12 15:13:15 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 from PIL import Image
 import plotly.express as px
 
-
+st.write('Hello')
 st.set_page_config(page_title="Proteomics Data for earlier Brain Cancer detection", page_icon="🧬")
 
 st.title('Using Machine Learning and Proteomics Data for earlier Brain Cancer detection')
