2b4e8787d0060b5e6973be1f968e7f8874a4a052
Klara Haas <klara-h@gmx.net>
2024-03-13 11:29:57 +0100

    Updated API

--- proteomics/api/preproc_input.py
+++ proteomics/api/preproc_input.py
@@ -26,7 +26,7 @@ def preprocess_input(data) -> np.array:
     data_clean= clean_data(data)
 
     # define X
-    X = data_clean.drop(['Case', 'gender', 'histological_type', 'race', 'ethnicity', 'radiation_therapy', 'Grade', 'Mutation.Count', 'Percent.aneuploidy', 'IDH.status', 'outcome'], axis = 1)
+    X = data_clean.drop(['Case', 'gender', 'histological_type', 'race', 'ethnicity', 'radiation_therapy', 'Grade', 'Mutation.Count', 'Percent.aneuploidy', 'IDH.status'], axis = 1)
     P17_list = ['Syk_p', 'YAP_pS127_p', 'AR_p', 'ACC1_p', 'YAP_p',
         'HER3_pY1289_p', 'c-Kit_p', 'ACC_pS79_p', 'STAT3_pY705_p', 'DJ-1_p',
         '53BP1_p', 'p27_p', 'PDK1_p', 'S6_pS235_S236_p', 'PRDX1_p', 'Bax_p', 'IRS1_p']
