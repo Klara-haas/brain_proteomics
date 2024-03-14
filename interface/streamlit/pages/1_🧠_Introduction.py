02c242e83deaeef572386c2f59a10b3b08a00b1f
Jana Schulz <felicitasschulz.js@gmail.com>
2024-03-14 12:46:35 +0100

    added tabs to intro

--- interface/streamlit/pages/1_🧠_Introduction.py
+++ interface/streamlit/pages/1_🧠_Introduction.py
@@ -16,108 +16,103 @@ st.title('Introduction')
 st.write("")
 st.write("")
 
-##### Glial cells and Gliomas ##################################################
-col1, col2 = st.columns([1.5, 2.5])
+# Create a tabbed layout
+tabs = st.tabs(['Glioma', 'Problem', 'Solution'])
 
-with col1:
-    st.header("Glial cells and Gliomas")
+with tabs[0]:
+    ##### Glial cells and Gliomas ##################################################
+    col1, col2 = st.columns([1.5, 2.5])
 
-    st.write("")
+    with col1:
+        st.header("Glial cells and Gliomas")
 
-    intro_items1 = ["Glial cells: support neurons in the brain",
-                "Gliomas: glial tumors",
-                "80% of malignant brain tumors",
-                "Classification based on affected cell type"
-                ]
-    st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items1]), unsafe_allow_html=True)
+        st.write("")
 
-with col1:
-    cola, colb = st.columns([0.2, 2])
-    with colb:
-        intro_items12 = [ "Oligodendroglioma",
-                    "Astrocytoma (more aggressive)"
-                        ]
-        st.markdown("\n".join([f"- <span style='font-size: 20px'>{item}</span>" for item in intro_items12]), unsafe_allow_html=True)
-
-with col1:
-    intro_items2 = [
-                    "Require different treatments"
+        intro_items1 = ["Glial cells: support neurons in the brain",
+                    "Gliomas: glial tumors",
+                    "80% of malignant brain tumors",
+                    "Classification based on affected cell type"
                     ]
-    st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items2]), unsafe_allow_html=True)
+        st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items1]), unsafe_allow_html=True)
+
+    with col1:
+        cola, colb = st.columns([0.2, 2])
+        with colb:
+            intro_items12 = [ "Oligodendroglioma",
+                        "Astrocytoma (more aggressive)"
+                            ]
+            st.markdown("\n".join([f"- <span style='font-size: 20px'>{item}</span>" for item in intro_items12]), unsafe_allow_html=True)
+
+    with col1:
+        intro_items2 = [
+                        "Require different treatments"
+                        ]
+        st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items2]), unsafe_allow_html=True)
 
 
-with col2:
-    path = os.path.dirname(__file__)
-    image1_path = os.path.join(path,"..", "images","750px-Blausen_0870_TypesofNeuroglia_marked_red.png")
-    #print("findme2", image1_path)
-    image1 = Image.open(image1_path)
-    st.image(image1, use_column_width=True)
+    with col2:
+        path = os.path.dirname(__file__)
+        image1_path = os.path.join(path,"..", "images","750px-Blausen_0870_TypesofNeuroglia_marked_red.png")
+        #print("findme2", image1_path)
+        image1 = Image.open(image1_path)
+        st.image(image1, use_column_width=True)
 
 
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
+
 
 ##### The Problem ##############################################################
 
-col3, col4 = st.columns([2, 2], gap = "large")
+with tabs[1]:
+    col3, col4 = st.columns([2, 2], gap = "large")
 
-with col3:
-    image2_path = os.path.join(path,"..", "images","41374_2003_Article_BF3780627_Fig1_HTML.jpg")
+    with col3:
+        st.write("")
+        st.write("")
+        image2_path = os.path.join(path,"..", "images","41374_2003_Article_BF3780627_Fig1_HTML.jpg")
 
-    image2 = Image.open(image2_path)
-    st.image(image2, caption='Histopathologic Classification of Gliomas', use_column_width=True)
+        image2 = Image.open(image2_path)
+        st.image(image2, caption='Histopathologic Classification of Gliomas', use_column_width=True)
 
-with col4:
-    st.header("The Problem")
+    with col4:
+        st.write("")
 
-    st.write("")
+        st.header("The Problem")
 
-    intro_items1 = ["Difficult classification with imaging",
+        st.write("")
 
-                    "Requires histological examination / biopsy",
-                    "High variability between different cancer types"
-                ]
-    st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items1]), unsafe_allow_html=True)
+        intro_items1 = ["Difficult classification with imaging",
 
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
+                        "Requires histological examination / biopsy",
+                        "High variability between different cancer types"
+                    ]
+        st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items1]), unsafe_allow_html=True)
+
+
 
 ##### The Solution #############################################################
+with tabs[2]:
+    col5, col6 = st.columns([1.5, 2.5], gap = "large")
 
-col5, col6 = st.columns([1.5, 2.5], gap = "large")
+    with col6:
+        st.write("")
+        st.write("")
 
-with col6:
-    image2_path = os.path.join(path,"..", "images","brain_proteomics_Wingo_et_al_2019.png")
+        image2_path = os.path.join(path,"..", "images","brain_proteomics_Wingo_et_al_2019.png")
 
-    image2 = Image.open(image2_path)
-    st.image(image2, use_column_width=True)
+        image2 = Image.open(image2_path)
+        st.image(image2, use_column_width=True)
 
-with col5:
-    st.header("The Solution")
+    with col5:
+        st.write("")
+        st.header("The Solution")
 
-    st.write("")
+        st.write("")
 
-    intro_items1 = [
-        "Measure proteins in the blood (less invasive)",
-        "Use machine learning"
-        "Faster diagnosis",
-        "More targeted treatment",
-        "Better patient outcomes"
-        ]
-    st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items1]), unsafe_allow_html=True)
+        intro_items1 = [
+            "Measure proteins in the blood (less invasive)",
+            "Use machine learning"
+            "Faster diagnosis",
+            "More targeted treatment",
+            "Better patient outcomes"
+            ]
+        st.markdown("\n".join([f"- <span style='font-size: 22px'>{item}</span>" for item in intro_items1]), unsafe_allow_html=True)
